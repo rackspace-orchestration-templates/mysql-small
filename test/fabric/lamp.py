bba3295fc1bed573ddeb3720390e5220e98d94ce
Chris Hultin <chris.hultin@rackspace.com>
2016-01-20 10:15:33 -0600

    Changing image to use proper name

--- test/fabric/lamp.py
+++ test/fabric/lamp.py
@@ -7,21 +7,14 @@ from hot.utils.test import get_artifacts
 def check():
   env.platform_family = detect.detect()
 
-  assert port.is_listening(80), 'port 80/apache is not listening'
+  assert port.is_listening(3306), 'port 3306/mysql is not listening'
 
   if (env.platform_family == "rhel"):
     assert process.is_up('httpd'), 'apache is not running'
-    assert process.is_up('php'), 'php is not running'
     assert service.is_enabled('httpd'), 'apache is not enabled'
-    assert service.is_enabled('php'), 'php is not enabled'
   elif (env.platform_family == 'debian'): 
-    assert process.is_up('apache2'), 'apache is not running' 
-    assert process.is_up('php5'), 'php is not running'
-    assert service.is_enabled('apach2e'), 'apache is not enabled'
-    assert service.is_enabled('php5'), 'php is not enabled' 
-  if ("secondary" not in socket.gethostname()):
-    assert process.is_up('lsyncd'), 'lsyncd is not running'
-    assert service.is_enabled('lsyncd'), 'lsyncd is not enabled'
+    assert process.is_up('mysql'), ' is not running'
+    assert service.is_enabled('mysql'), 'mysql is not enabled' 
 
 @task
 def artifacts():
